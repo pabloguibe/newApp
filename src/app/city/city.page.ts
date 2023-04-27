b76b91af57835f17190524112ecb15a9c8ee50f9
Sebastiá Guisasola <sguisasola@dreampulse.es>
2023-04-27 13:52:13 +0200

    cityPage

--- src/app/city/city.page.ts
+++ src/app/city/city.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-city',
@@ -13,12 +15,34 @@ export class CityPage implements OnInit {
 
 
   id: any;
+  finalId!: number;
+  cities: any = [];
+  name!: string;
+  image!:string;
+  desc!:string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
-    this.id=this.activatedRoute.snapshot.paramMap.get("id");
+    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+    this.finalId = this.id -1;
     console.log("id", this.id);
+
+    this.getCities().subscribe(res => {
+      console.log("Res", res)
+      this.cities = res;
+      this.name= this.cities[this.finalId].name;
+      this.image= this.cities[this.finalId].image;
+      this.desc= this.cities[this.finalId].desc;
+      console.log("Name", this.name)
+    })
+
+  }
+
+  getCities() {
+    return this.http.get("assets/files/cities.json").pipe(map((res: any) => {
+      return res.data;
+    }))
   }
 
 }
